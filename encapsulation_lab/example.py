90e6841fe840b27650117c9f62749ef360a1b7d3
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2023-03-07 16:06:33 +0200

    Added more solutions

--- encapsulation_lab/example.py
+++ encapsulation_lab/example.py
@@ -2,16 +2,15 @@ class Employee:
     name = 'Diyan'
     salary = '25000'
 
+    def __getattribute__(self, attr):
+        return None
+
     def show(self):
         print(self.name)
         print(self.salary)
 
 
 employee = Employee()
-print(getattr(employee, 'name'))   # Diyan
-print(hasattr(employee, 'name'))   # True
-setattr(employee, 'height', 152)
-print(getattr(employee, 'height')) # 152
-delattr(Employee, 'salary')
+print(employee.name)
 
 a = 5
